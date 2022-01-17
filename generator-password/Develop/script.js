ec3db0151a65e37cfe4cc1077c8f3a7e9af42f21
Dknowles <dknowles4489@gmail.com>
2022-01-16 22:38:24 -0500

    added Random func

--- generator-password/Develop/script.js
+++ generator-password/Develop/script.js
@@ -1,6 +1,28 @@
 // Assignment code here
+const randomFunc = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+// Generator functions
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()_+-=';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
