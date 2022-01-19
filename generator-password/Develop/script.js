c4b2f9260265d651b7cfbd974f77d18dda403a4f
Dknowles <dknowles4489@gmail.com>
2022-01-19 18:48:22 -0500

    function finished

--- generator-password/Develop/script.js
+++ generator-password/Develop/script.js
@@ -19,6 +19,45 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
+var randomProperty = function (obj) {
+  var keys = Object.keys(obj);
+  return obj[keys[ keys.length * Math.random() << 0]];
+};
+
+function generatePassword() { 
+var length = window.prompt('How long do you want your password between 8 and 128', '12');
+var upper = window.confirm('Would you like uppercase letters ?');
+var lower = window.confirm('Would you like lowercase letters ?');
+var symbol = window.confirm('Would you like symbols ?');
+var numb = window.confirm('Would you like numbers ?');
+
+var finalpass = '';
+var funcpool = {};
+if(upper) {
+  funcpool.upper = getRandomUpper;
+}
+if(lower) {
+  funcpool.lower = getRandomLower;
+}
+if(symbol) {
+  funcpool.symbol = getRandomSymbol;
+}
+if(numb) {
+  funcpool.numb = getRandomNumber;
+}
+
+console.log(funcpool);
+
+for(var i = 0; i < length; i++) {
+  console.log(randomProperty(funcpool))
+  finalpass += randomProperty(funcpool)()
+
+}
+
+return finalpass;
+}
+
+
 function getRandomSymbol() {
   const symbols = '!@#$%^&*()_+-=';
   return symbols[Math.floor(Math.random() * symbols.length)];
